0d26a48d0b766d2ee629a8f9f84a9ebd1bea0bb0
Santhosh G <santhoshg0004@gmail.com>
2025-08-25 09:23:52 +0530

    Implemented file read capability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{fs::File, path::Path};
+
+use crate::model::task::Task;
+mod model;
 fn main() {
-    println!("Hello, world!");
+    let json_file_path = Path::new(".tasks.json");
+    let file = File::open(json_file_path).expect("Failed to open .tasks.json file");
+
+    let tasks: Vec<Task> = serde_json::from_reader(file).expect("Failed to parse JSON");
+
+    for task in tasks {
+        println!("{:?}", task);
+    }
 }
