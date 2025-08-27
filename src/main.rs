6ac370ec510656794ffad6ab5a10a46b3ec0cc68
Santhosh G <santhoshg0004@gmail.com>
2025-08-27 10:38:46 +0530

    Implemented write logic, now we write into files, and also implemented file Initialization logic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
+use core::task;
 use std::{fs::File, path::Path};
 
 use crate::model::task::Task;
 mod model;
 fn main() {
     let json_file_path = Path::new(".tasks.json");
-    let file = File::open(json_file_path).expect("Failed to open .tasks.json file");
+    let file: File;
 
-    let tasks: Vec<Task> = serde_json::from_reader(file).expect("Failed to parse JSON");
+    if (json_file_path.exists()) == false {
+        println!(".tasks.json file not found");
+        println!("Initializing task manager...");
 
-    for task in tasks {
-        println!("{:?}", task);
+        File::create(json_file_path).expect("Failed to create .tasks.json file");
+
+        return;
     }
+    file = File::open(json_file_path).expect("Failed to open .tasks.json file");
+
+    let mut tasks: Vec<Task> = serde_json::from_reader(file).expect("Failed to parse JSON");
+
+    tasks.iter().for_each(|task| {
+        println!("{:?}", task);
+    });
+
+    let new_task = Task {
+        id: 11,
+        title: String::from("Sample Task"),
+        description: String::from("This is a sample task description."),
+        completed: false,
+    };
+    println!("New Task: {:?}", new_task);
+
+    tasks.push(new_task);
+
+    tasks.iter().for_each(|task| {
+        println!("{:?}", task);
+    });
 }
